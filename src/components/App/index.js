edd0ba9befc2a26e6d4ec40799e6a81191039771
Maciej Sykuła <maciek.sykula@gmail.com>
2019-11-07 09:42:38 +0100

    Update routes and add styling to the App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Router } from '@reach/router';
 
 import Form from '../../routes/Form';
+import Results from '../../routes/Results';
+
+import './style.scss';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Form path="/" />
+        <Results path="/results" />
       </Router>
     </div>
   );
