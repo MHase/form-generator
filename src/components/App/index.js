73229fa2c2f1a82fdae818ae79fa3c724bf13a09
Maciej Sykuła <maciek.sykula@gmail.com>
2019-11-07 08:58:38 +0100

    Add Form route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import Form from '../../routes/Form';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Router>
+        <Form path="/" />
+      </Router>
+    </div>
   );
 }
 
